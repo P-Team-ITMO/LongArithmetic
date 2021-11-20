b6bb3c76ef405dbd6dc14e0dd2de780676df69c0
Sherka <Obidjanow@mail.ru>
2021-11-21 00:18:02 +0300

    Start Lab1. Add: bytes number options

---
+++ Lab1/main.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+int main(int argc, char *argv[])
+{
+	setlocale(LC_ALL, "ru");
+	char f[1000000];
+	int bytes = 0, words=0, lines=0, i=1;
+	FILE* file = fopen(argv[2],"r");
+	if (strncmp(argv[1], "-c") == 0 || strncmp(argv[1], "--bytes") == 0)
+	{
+		while (fscanf(file, "%c", f) != EOF)
+			bytes++; 
+		printf("%d", bytes);
+	}
+	return 0;
+}
