40bd9d7aa907a99edb8442a6afdcc873abeea434
Iamnotagenius <kazakk9@gmail.com>
2021-11-21 16:44:55 +0300

    Tests for WordCount

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,13 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
+#include <string.h>
 int main(int argc, char *argv[])
 {
 	setlocale(LC_ALL, "ru");
 	char f[1000000];
 	int bytes = 0, words=0, lines=0, i=1;
 	FILE* file = fopen(argv[2],"r");
-	if (strncmp(argv[1], "-c") == 0 || strncmp(argv[1], "--bytes") == 0)
+	if (!file) {
+		fprintf(stderr, "%s: %s: No such file or directory\n", argv[0], argv[2]);
+	}
+	if (strcmp(argv[1], "-c") == 0 || strcmp(argv[1], "--bytes") == 0)
 	{
 		while (fscanf(file, "%c", f) != EOF)
 			bytes++; 
