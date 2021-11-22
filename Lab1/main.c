38521c4d19e23498377c7864fe96f90f2c3f05fe
Sherka <Obidjanow@mail.ru>
2021-11-22 23:09:52 +0300

    Add: line number function

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,27 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
-#include <string.h>
 int main(int argc, char *argv[])
 {
 	setlocale(LC_ALL, "ru");
 	char f[1000000];
 	int bytes = 0, words=0, lines=0, i=1;
 	FILE* file = fopen(argv[2],"r");
-	if (!file) {
-		fprintf(stderr, "%s: %s: No such file or directory\n", argv[0], argv[2]);
-	}
-	if (strcmp(argv[1], "-c") == 0 || strcmp(argv[1], "--bytes") == 0)
+	if (strncmp(argv[1], "-c") == 0 || strncmp(argv[1], "--bytes") == 0)
 	{
 		while (fscanf(file, "%c", f) != EOF)
 			bytes++; 
 		printf("%d", bytes);
 	}
 	else if (strncmp(argv[1], "-w") == 0 || strncmp(argv[1], "--words") == 0)
-		{
-			while (fscanf(file, "%s", f) != EOF)
-				words++;
-			printf("%d", words);
-		}
+	{
+		while (fscanf(file, "%s", f) != EOF)
+			words++;
+		printf("%d", words);
+	}
+	else if (strncmp(argv[1], "-l") == 0 || strncmp(argv[1], "--lines") == 0)
+	{
+		while (fgets(f,10000,file)!=NULL)
+			lines++;
+		printf("%d",lines);
+	}
 	return 0;
 }
