98949d9ad370e0feb0e82734d5f63f6e652b86b2
Sherka <Obidjanow@mail.ru>
2021-11-23 21:26:26 +0300

    timeFromString

--- Lab3/mail.c
+++ Lab3/mail.c
@@ -23,6 +23,25 @@ FILE *getFile(char *filename) {
         error(FILE_NOT_FOUND);
     return input;
 }
+time_t timeFromString(char *str) {
+    if (str == NULL)
+        return 0;
+    char months[12][4] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    struct tm time = {0};
+    for (int i = 0; i < 12; ++i)
+        if (!strcmp(months[i], str + 3)) {
+            time.tm_mon = i;
+            break;
+        }
+    time.tm_year = atoi(str + 7) - 1900;
+    time.tm_mday = atoi(str);
+    time.tm_hour = atoi(str + 12);
+    time.tm_min = atoi(str + 15);
+    time.tm_sec = atoi(str + 18);
+    time.tm_isdst = -1;
+    time.tm_gmtoff = ((str[22] - '0') * 10 + (str[23] - '0')) * 3600 + ((str[24] - '0') * 10 + (str[25] - '0')) * 60;
+    return mktime(&time);
+}
 int main(int argc, char **argv) {
     return 0;
 }
